64afd80ef4b4bb208a72f51ee4704e3c254680c9
Rafael <rafaelmsabino@gmail.com>
2018-11-26 13:43:52 -0200

    add FeignClient

--- src/main/java/br/edu/ulbra/election/voter/Client/ElectionClient.java
+++ src/main/java/br/edu/ulbra/election/voter/Client/ElectionClient.java
@@ -1,4 +1,14 @@
 package br.edu.ulbra.election.voter.Client;
 
+import br.edu.ulbra.election.voter.output.v1.ElectionOutput;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(value = "election-service", url = "${url.election-service}")
 public interface ElectionClient {
+    @GetMapping("/v1/voter/{voterId}")
+    List<ElectionOutput> getByVoterId(@PathVariable(name = "voterId") Long voterId);
 }
