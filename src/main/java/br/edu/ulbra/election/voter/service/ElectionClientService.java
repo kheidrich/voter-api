49b616233ca2cc61e5aadfa335afddd7cc28de14
Rafael <rafaelmsabino@gmail.com>
2018-11-26 13:45:07 -0200

    add ElectionClientService

--- src/main/java/br/edu/ulbra/election/voter/service/ElectionClientService.java
+++ src/main/java/br/edu/ulbra/election/voter/service/ElectionClientService.java
@@ -1,4 +1,22 @@
 package br.edu.ulbra.election.voter.service;
 
+import br.edu.ulbra.election.voter.Client.ElectionClient;
+import br.edu.ulbra.election.voter.output.v1.ElectionOutput;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ElectionClientService {
+    private final ElectionClient electionClient;
+
+    @Autowired
+    public ElectionClientService(ElectionClient electionClient){
+        this.electionClient = electionClient;
+    }
+
+    public List<ElectionOutput> getVoterId(Long voterId){
+        return this.electionClient.getByVoterId(voterId);
+    }
 }
